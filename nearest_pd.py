036fb439da3c889573c8cc6e4cd1671fce6c6778
Eero Siivola <eero.siivola@aalto.fi>
2020-04-03 09:06:53 +0300

    Added the source files

---
+++ nearest_pd.py
@@ -0,0 +1,98 @@
+'''
+The original implementation is done by John D'Errico: https://www.mathworks.com/matlabcentral/fileexchange/42885-nearestspd
+with the following licence:
+
+Copyright (c) 2013, John D'Errico
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The implementation is ported to python by Ahmed Fasih
+nearestPD and isPD are copied from https://github.com/alan-turing-institute/bocpdms with the same licence as John D'Errico.
+
+
+This implementation is just a minor modification to the python implementation to make it importable without copy paste
+'''
+
+def nearestPD(A: np.ndarray) -> Tuple[np.ndarray, float]:
+    """
+    Find the nearest positive-definite matrix to input A.
+
+    A Python/Numpy port of John D'Errico's `nearestSPD` MATLAB code [1], which
+    credits [2].
+
+    [1] https://www.mathworks.com/matlabcentral/fileexchange/42885-nearestspd
+
+    [2] N.J. Higham, "Computing a nearest symmetric positive semidefinite
+    matrix" (1988): https://doi.org/10.1016/0024-3795(88)90223-6
+    
+    :param A: A square matrix
+    :return: A Tuple containing the positive definite version of A and a float
+                 indicating how much jitter was added to the diagonal of A
+                 to make it positive definite 
+    """
+
+    B = (A + A.T) / 2
+    _, s, V = la.svd(B)
+
+    H = np.dot(V.T, np.dot(np.diag(s), V))
+
+    A2 = (B + H) / 2
+
+    A3 = (A2 + A2.T) / 2
+
+    k = 1
+    if isPD(A3):
+        return A3, 0       
+
+    spacing = np.spacing(la.norm(A))
+    # The above is different from [1]. It appears that MATLAB's `chol` Cholesky
+    # decomposition will accept matrixes with exactly 0-eigenvalue, whereas
+    # Numpy's will not. So where [1] uses `eps(mineig)` (where `eps` is Matlab
+    # for `np.spacing`), we use the above definition. CAVEAT: our `spacing`
+    # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually on
+    # the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
+    # `spacing` will, for Gaussian random matrixes of small dimension, be on
+    # othe order of 1e-16. In practice, both ways converge, as the unit test
+    # below suggests.
+    I = np.eye(A.shape[0])
+    
+    mineig = 0.0
+    while not isPD(A3):
+        mineig = np.min(np.real(la.eigvals(A3)))
+        A3 += I * (-mineig * k**2 + spacing)
+        k += 1
+        if(k>10):
+            break   
+    return A3, -mineig * k**2 + spacing
+
+def isPD(B: np.ndarray) -> bool:
+    """
+    Returns true when input is positive-definite, via Cholesky
+    
+    :param B: a square matrix
+    :return: A boolean value indicating if B is positive definite
+    """
+    try:
+        _ = la.cholesky(B)
+        return True
+    except la.LinAlgError:
+        return False
